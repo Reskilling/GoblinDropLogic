49c39b4d87c06a85f394fe8efc2de4c0fadd920f
DWilliams <devanwilliams1972@gmail.com>
2026-04-06 10:01:03 -0700

    feat: Reset KC to 0 and fold away the old chart on ANY boss change

--- js/app.js
+++ js/app.js
@@ -514,10 +514,15 @@ function initGoalTracking() {
 
 function handleBossSelection(e) {
     activeBossKey = e.target.value;
+    
+    if (DOM.kcInput) DOM.kcInput.value = 0;
+    DOM.resultsSection.classList.add("hidden");
+
     if (!activeBossKey) { 
         DOM.bossPreview.classList.add("hidden"); 
         return; 
     }
+    
     renderItemGrid(BOSS_CONFIG[activeBossKey].items);
     renderDynamicSettings(activeBossKey);
     DOM.bossPreview.classList.remove("hidden");
